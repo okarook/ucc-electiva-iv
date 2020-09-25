4693cc13d8cf456300d20d40b0c23134ab222b62
Oscar Tovar <okarook@gmail.com>
2020-09-25 10:47:51 -0500

    Se agrega el codigo fuente del proyecto 05

--- 05-pushbutton-complejo/src/main.ino
+++ 05-pushbutton-complejo/src/main.ino
@@ -1,18 +1,35 @@
-int led1 = 13;
-int led2 = 12;
-int switchA = 11;
-bool state;
+int pinSwitchA = 10;
+int pinSwitchB = 11;
+int pinLedA = 13;
+int pinLedB = 12;
+bool valueSwitchA;
+bool valueSwitchB;
+bool stateA = false;
+bool stateB = false;
 
 void setup()
 {
-  pinMode(switchA, INPUT_PULLUP);
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
+  pinMode(pinSwitchA, INPUT_PULLUP);
+  pinMode(pinSwitchB, INPUT_PULLUP);
+  pinMode(pinLedA, OUTPUT);
+  pinMode(pinLedB, OUTPUT);
 }
 
 void loop()
 {
-  state = digitalRead(switchA);
-  digitalWrite(led1, state);
-  digitalWrite(led2, ! state);
+  valueSwitchA = digitalRead(pinSwitchA);
+  valueSwitchB = digitalRead(pinSwitchB);
+
+  if (! valueSwitchA) {
+    stateA= !stateA;
+    digitalWrite(pinLedA, stateA);
+    digitalWrite(pinLedB, stateA);
+  }
+
+  if (! valueSwitchB) {
+    stateB = !stateB;
+    digitalWrite(pinLedA, stateB);
+  }
+
+  delay(200);
 }
